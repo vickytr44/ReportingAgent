782c914878acf85f30b6aa8c381a8e19bc4eafc4
Vickraman Thekkan Ravi <vravi@devon.nl>
2025-03-09 20:44:46 +0530

    fetch schema via api

--- src/schema_provider.py
+++ src/schema_provider.py
@@ -1,26 +1,20 @@
-from account_schema_graphql import escaped_account_schema_graphql, account_schema_graphql
-from customer_schema_graphql import escaped_customer_schema_graphql, customer_schema_graphql
-from bill_schema_graphql import escaped_bill_schema_graphql, bill_schema_graphql
 from entity_constants import *
+from graphql_client import execute_graphql_query
 
 def get_escaped_schema(main_entity):
-    schema_mapping = {
-        ACCOUNTS: escaped_account_schema_graphql,
-        CUSTOMERS: escaped_customer_schema_graphql,
-        BILLS: escaped_bill_schema_graphql
-    }
     try:
-        return schema_mapping[main_entity]
+        schema = get_schema(main_entity).replace("{", "{{").replace("}", "}}")
+        print(schema)
+        return schema
     except KeyError:
         raise ValueError("Unknown main entity")
 
 def get_schema(main_entity):
-    schema_mapping = {
-        ACCOUNTS: account_schema_graphql,
-        CUSTOMERS: customer_schema_graphql,
-        BILLS: bill_schema_graphql
-    }
+    query: str = "{" + f"""cleanedSchema(schemaName: "{main_entity}")""" + "}"
     try:
-        return schema_mapping[main_entity]
+        result = execute_graphql_query(query).get("cleanedSchema")
+        print("result:", result)
+        return result
     except KeyError:
         raise ValueError("Unknown main entity")
+    
